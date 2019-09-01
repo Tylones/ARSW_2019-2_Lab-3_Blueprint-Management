df07e7cf3635c8f514068f8aca0f16cc777cf037
Tylones <etiennereitz@gmail.com>
2019-09-02 01:19:41 +0200

    Modified application context, and BPServices. Now bean injection works

--- src/main/java/edu/eci/arsw/blueprints/main/mainProgram.java
+++ src/main/java/edu/eci/arsw/blueprints/main/mainProgram.java
@@ -1,11 +1,29 @@
 package edu.eci.arsw.blueprints.main;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import edu.eci.arsw.blueprints.model.Blueprint;
+import edu.eci.arsw.blueprints.model.Point;
+import edu.eci.arsw.blueprints.persistence.BlueprintNotFoundException;
+import edu.eci.arsw.blueprints.persistence.BlueprintPersistenceException;
+import edu.eci.arsw.blueprints.persistence.impl.InMemoryBlueprintPersistence;
+import edu.eci.arsw.blueprints.services.BlueprintsServices;
 
 public class mainProgram{
-    
-    public static void main(String[] args){
 
-        System.out.println("Main working");
+    public static void main(String[] args) throws BlueprintNotFoundException, BlueprintPersistenceException{
+        ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
+        BlueprintsServices bps = ac.getBean(BlueprintsServices.class);
+
+        Point[] pts0=new Point[]{new Point(40, 40),new Point(15, 15)};
+        Blueprint bp0=new Blueprint("mack", "mypaint",pts0);
+
+        bps.addNewBlueprint(bp0);
+
+        bps.getBlueprint("mack", "mypaint");
+        
+        
 
     }
 
